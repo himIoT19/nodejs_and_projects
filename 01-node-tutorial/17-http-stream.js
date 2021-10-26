2b747c00cb44b3670ce4a3be21ef86ff813db031
himIoT19 <thehimu1990@gmail.com>
2021-10-26 10:45:29 +0530

    Complete: Node basics.

--- 01-node-tutorial/17-http-stream.js
+++ 01-node-tutorial/17-http-stream.js
@@ -2,11 +2,12 @@ var http = require("http");
 var fs = require("fs");
 
 http
-  .createServer(function (req, res) {
+  .createServer((req, res) => {
     // const text = fs.readFileSync('./content/big.txt', 'utf8')
     // res.end(text)
     const fileStream = fs.createReadStream("./content/big.txt", "utf8");
     fileStream.on("open", () => {
+      // Write data in chunks using 'pipe'
       fileStream.pipe(res);
     });
     fileStream.on("error", (err) => {
